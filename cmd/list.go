85b550730863986624017ba251685de4d8c2231f
Matt Merkes <merkes@amazon.com>
2020-01-21 08:35:01 -0800

    Adds list command to print out tasks

---
+++ cmd/list.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/mmerkes/clerk/pkg/storage"
+	"github.com/spf13/cobra"
+)
+
+var Verbose bool
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List tasks in the console",
+	Long:  `Prints a list of tasks in the console.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		storage.ListTasks(Verbose)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+}
